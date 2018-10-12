0f548829481ee6b1d8333981332b0ba2e2789610
George Webster <george@ethyreal.com>
2018-10-12 15:42:49 -0700

    Added initial exercise unit tests
    also added struct with stubs, not true TDD as we could test without the Bottles struct existing but Xcode sometimes freaks out if it can’t find files so this is just easier.
    
    all tests disable execpt the first one so the exercise is ready to being

---
+++ NinetyNineBottles/Bottles.swift
@@ -0,0 +1,24 @@
+//
+//  Bottles.swift
+//  NinetyNineBottles
+//
+//  Created by George Webster on 10/12/18.
+//  Copyright © 2018 George Webster. All rights reserved.
+//
+
+import Foundation
+
+struct Bottles {
+    
+    func verse(_ count:Int) -> String {
+        return ""
+    }
+    
+    func verses(_ start:Int, to end:Int) -> String {
+        return ""
+    }
+    
+    func song() -> String {
+        return ""
+    }
+}
