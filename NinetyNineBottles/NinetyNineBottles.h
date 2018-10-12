64c68d6d06ef15dd93234110694821a275029308
George Webster <george@ethyreal.com>
2018-10-12 15:24:56 -0700

    Initial Commit

---
+++ NinetyNineBottles/NinetyNineBottles.h
@@ -0,0 +1,19 @@
+//
+//  NinetyNineBottles.h
+//  NinetyNineBottles
+//
+//  Created by George Webster on 10/12/18.
+//  Copyright © 2018 George Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NinetyNineBottles.
+FOUNDATION_EXPORT double NinetyNineBottlesVersionNumber;
+
+//! Project version string for NinetyNineBottles.
+FOUNDATION_EXPORT const unsigned char NinetyNineBottlesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NinetyNineBottles/PublicHeader.h>
+
+
