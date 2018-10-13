dc19b0ff563e8fed97c625ac29d2f186d7fe5284
George Webster <george@ethyreal.com>
2018-10-13 14:13:40 -0700

    added a “Shameless Green” implementation
    very similar to section 1.1.4 shameless green found in 99bottles

--- NinetyNineBottles/Bottles.swift
+++ NinetyNineBottles/Bottles.swift
@@ -11,14 +11,34 @@ import Foundation
 struct Bottles {
     
     func verse(_ count:Int) -> String {
-        return ""
+        switch count {
+        case 2:
+            return "2 bottles of beer on the wall, 2 bottles of beer.\n"
+                + "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+        case 1:
+            return "1 bottle of beer on the wall, 1 bottle of beer.\n"
+                + "Take it down and pass it around, no more bottles of beer on the wall.\n"
+        case 0:
+            return "No more bottles of beer on the wall, no more bottles of beer.\n"
+                + "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        default:
+            return "\(count) bottles of beer on the wall, \(count) bottles of beer.\n"
+                + "Take one down and pass it around, \(count - 1) bottles of beer on the wall.\n"
+        }
     }
     
     func verses(_ start:Int, to end:Int) -> String {
-        return ""
+        guard start >= end else { return "" }
+        var verses = [String]()
+        for x in end...start {
+            verses.append(verse(x))
+        }
+        return verses
+            .reversed()
+            .joined(separator: "\n")
     }
     
     func song() -> String {
-        return ""
+        return verses(99, to: 0)
     }
 }
